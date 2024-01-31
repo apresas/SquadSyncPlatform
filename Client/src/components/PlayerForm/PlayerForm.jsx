c1f9dd4df203d86b88354643f824eff5a4c2ae8b
apresas <a.presas@outlook.com>
2024-01-30 20:38:46 -0800

    rework onchange for add players form

--- Client/src/components/PlayerForm/PlayerForm.jsx
+++ Client/src/components/PlayerForm/PlayerForm.jsx
@@ -19,11 +19,8 @@ function PlayerForm({ teamData }) {
 
   const shootsCatches = [{ name: "L" }, { name: "R" }];
 
-  const numbers = Array.from({ length: 99 }, (_, i) => i + 1);
-
-  const jerseyNumbers = numbers.map((n) => ({ name: n }));
-
-  // console.log(numbers);
+  // const numbers = Array.from({ length: 99 }, (_, i) => i + 1);
+  // const jerseyNumbers = numbers.map((n) => ({ name: n }));
 
   const [newPlayer, setNewPlayer] = useState({
     playerID: 0,
@@ -44,7 +41,6 @@ function PlayerForm({ teamData }) {
   const [selectedClass, setSelectedClass] = useState("");
   const [newPlayerID, setNewPlayerID] = useState(0);
   const [selectedHandedness, setSelectedHandedness] = useState();
-  const [selectedJerseyNumber, setSelectedJerseyNumber] = useState("");
   const [playerFirstName, setPlayerFirstName] = useState("");
   const [playerLastName, setPlayerLastName] = useState("");
   const [playerHeight, setPlayerHeight] = useState();
@@ -56,45 +52,72 @@ function PlayerForm({ teamData }) {
     setNewPlayerID(newID);
   }, []);
 
+  const generateID = () => {
+    const newID = Math.floor(100000 + Math.random() * 900000);
+    setNewPlayerID(newID);
+  };
+
+  useEffect(() => {
+    setNewPlayer({
+      playerID: newPlayerID,
+      firstName: playerFirstName,
+      lastName: playerLastName,
+      height: playerHeight,
+      weight: playerWeight,
+      jerseyNumber: playerJerseyNumber,
+      teamID: selectedTeamID,
+      handedness: selectedHandedness,
+      position: selectedPosition,
+      class: selectedClass,
+    });
+  }, [
+    newPlayerID,
+    playerFirstName,
+    playerLastName,
+    playerHeight,
+    playerWeight,
+    playerJerseyNumber,
+    selectedTeamID,
+    selectedPosition,
+    selectedHandedness,
+    selectedClass,
+  ]);
 
   const submitPlayer = async (e) => {
     e.preventDefault();
-    // setNewPlayer({
-    //   playerID:  newPlayerID,
-    //   firstName: playerFirstName,
-    //   lastName: playerLastName,
-    //   height: playerHeight,
-    //   weight: playerWeight,
-    //   jerseyNumber: playerJerseyNumber,
-    //   position: selectedPosition,
-    //   class: selectedClass,
-    //   teamID: selectedTeamID,
-    // });
-
     try {
       await axios.post("http://localhost:9200/players", newPlayer);
       console.log("Player Added");
     } catch (err) {
       console.log(err);
-      console.log(newPlayer);
     }
+    // clearFields();
+  };
+
+  const clearFields = () => {
+    setPlayerFirstName("");
+    setPlayerLastName("");
+    setPlayerHeight(undefined);
+    setPlayerWeight(undefined);
+    setPlayerJerseyNumber(undefined);
+    generateID();
   };
 
   useEffect(() => {
     console.log(newPlayer);
   }, [newPlayer]);
 
-  const handleFormChange = (e) => {
-    setNewPlayer((player) => ({
-      ...player,
-      [e.target.name]: e.target.value,
-      playerID: newPlayerID,
-      position: selectedPosition,
-      handedness: selectedHandedness,
-      class: selectedClass,
-      teamID: selectedTeamID,
-    }));
-  };
+  // const handleFormChange = (e) => {
+  //   setNewPlayer((player) => ({
+  //     ...player,
+  //     [e.target.name]: e.target.value,
+  //     playerID: newPlayerID,
+  //     position: selectedPosition,
+  //     handedness: selectedHandedness,
+  //     class: selectedClass,
+  //     teamID: selectedTeamID,
+  //   }));
+  // };
 
   // console.log(newPlayer);
 
@@ -111,8 +134,6 @@ function PlayerForm({ teamData }) {
             type="text"
             disabled="disabled"
             value={newPlayerID}
-            // name="playerID"
-            onChange={handleFormChange}
           />
         </div>
         <div className="id_field field">
@@ -122,7 +143,6 @@ function PlayerForm({ teamData }) {
             disabled="disabled"
             value={selectedTeamID}
             name="teamID"
-            onChange={handleFormChange}
           />
         </div>
       </div>
@@ -132,8 +152,7 @@ function PlayerForm({ teamData }) {
           <input
             name="firstName"
             type="text"
-            onChange={handleFormChange}
-            // onChange={(e) => setPlayerFirstName(e.target.value)}
+            onChange={(e) => setPlayerFirstName(e.target.value)}
           />
         </div>
         <div className="field">
@@ -141,8 +160,7 @@ function PlayerForm({ teamData }) {
           <input
             name="lastName"
             type="text"
-            onChange={handleFormChange}
-            // onChange={(e) => setPlayerLastName(e.target.value)}
+            onChange={(e) => setPlayerLastName(e.target.value)}
           />
         </div>
       </div>
@@ -152,8 +170,7 @@ function PlayerForm({ teamData }) {
           <input
             name="height"
             type="number"
-            onChange={handleFormChange}
-            // onChange={(e) => setPlayerHeight(e.target.value)}
+            onChange={(e) => setPlayerHeight(e.target.value)}
           />
         </div>
         <div className="field">
@@ -161,8 +178,7 @@ function PlayerForm({ teamData }) {
           <input
             name="weight"
             type="number"
-            onChange={handleFormChange}
-            // onChange={(e) => setPlayerWeight(e.target.value)}
+            onChange={(e) => setPlayerWeight(e.target.value)}
           />
         </div>
       </div>
@@ -171,8 +187,7 @@ function PlayerForm({ teamData }) {
         <input
           name="jerseyNumber"
           type="number"
-          onChange={handleFormChange}
-          // onChange={(e) => setPlayerJerseyNumber(e.target.value)}
+          onChange={(e) => setPlayerJerseyNumber(e.target.value)}
         />
       </div>
       <div className="field dropdown_field_container">
@@ -182,25 +197,21 @@ function PlayerForm({ teamData }) {
           setSelectedTeamID={setSelectedTeamID}
           currentTeamTitle={currentTeamTitle}
           setCurrentTeamTitle={setCurrentTeamTitle}
-          onChange={handleFormChange}
         />
         <PlayerDropdown
           data={positions}
           type="Position"
           setSelectedPosition={setSelectedPosition}
-          onChange={handleFormChange}
         />
         <PlayerDropdown
           data={shootsCatches}
           type="Handedness"
           setSelectedHandedness={setSelectedHandedness}
-          onChange={handleFormChange}
         />
         <PlayerDropdown
           data={years}
           type="Class"
           setSelectedClass={setSelectedClass}
-          onChange={handleFormChange}
         />
       </div>
       <div className="player_form_controls">
--- Client/src/pages/Archieve.jsx
+++ Client/src/pages/Archieve.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import PlayerForm from "../components/PlayerForm/PlayerForm";
+import SponcerBar from "../components/Sponcer/SponcerBar";
 import "./archieve.css";
 
 function Archieve({teamData}) {
   return (
+    <>
+    <SponcerBar />
+    <NavBar />
     <div className="archieve_container">
-      <NavBar />
       <div className="archieve_content_container">
         <div>Archieve</div>
         <PlayerForm teamData={teamData}/>
       </div>
       <Footer />
     </div>
+    </>
   );
 }
 
